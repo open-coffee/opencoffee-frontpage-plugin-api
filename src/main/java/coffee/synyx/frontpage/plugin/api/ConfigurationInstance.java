b4d9ccda55338d0e6fe0ea7b584bd30411c4ff77
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2018-04-15 20:18:27 +0200

    Add execution of sources and javadoc to build time

--- src/main/java/coffee/synyx/frontpage/plugin/api/ConfigurationInstance.java
+++ src/main/java/coffee/synyx/frontpage/plugin/api/ConfigurationInstance.java
@@ -5,6 +5,8 @@ public interface ConfigurationInstance {
     /**
      * Get configuration by given key
      *
+     * @param key to retrieve the value defined by the plugin
+     *
      * @return the value of the given key
      */
     String get(String key);
