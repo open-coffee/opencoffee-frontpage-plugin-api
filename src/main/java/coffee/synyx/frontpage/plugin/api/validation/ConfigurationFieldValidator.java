7b4c49601c5f7669d9996cd7db2c5fe4888ab3e7
Benjamin Seber <seber@synyx.de>
2018-04-27 14:20:30 +0200

    added ConfigurationFieldValidator interface

---
+++ src/main/java/coffee/synyx/frontpage/plugin/api/validation/ConfigurationFieldValidator.java
@@ -0,0 +1,27 @@
+package coffee.synyx.frontpage.plugin.api.validation;
+
+import coffee.synyx.frontpage.plugin.api.ConfigurationField;
+import org.springframework.validation.Errors;
+
+public interface ConfigurationFieldValidator {
+
+    /**
+     * checks whether this validator supports the given {@link ConfigurationField} or not
+     *
+     * @param field
+     * @return
+     *      <code>true</code> when this validator supports the given {@link ConfigurationField},
+     *      <code>false</code> otherwise
+     */
+    boolean supports(ConfigurationField field);
+
+    /**
+     * validates the given target value and adds an item to the {@link Errors}
+     * when validation fails
+     *
+     * @param target the item to validate against the field
+     * @param field the configuration field description to validate against
+     * @param errors the errors object that will receive the validation error
+     */
+    void validate(Object target, ConfigurationField field, Errors errors);
+}
