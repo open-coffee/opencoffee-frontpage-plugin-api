b494b5aad9468fe7937baf31909c5297a11d8bb9
Benjamin Seber <seber@synyx.de>
2018-04-27 14:20:52 +0200

    ConfigurationDescription should be iterable
    so we can do stuff like
    
    ```
    for (ConfigurationField field : myConfigurationDescription) {
      // ...
    }
    ```

--- src/main/java/coffee/synyx/frontpage/plugin/api/ConfigurationDescription.java
+++ src/main/java/coffee/synyx/frontpage/plugin/api/ConfigurationDescription.java
@@ -1,10 +1,13 @@
 package coffee.synyx.frontpage.plugin.api;
 
+import java.util.Iterator;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
-public interface ConfigurationDescription {
+public interface ConfigurationDescription extends Iterable<ConfigurationField> {
 
     /**
      * Returns a description of all configuration needed to configure the plugin.
@@ -12,6 +15,21 @@ public interface ConfigurationDescription {
      * @return the {@link ConfigurationField}s of a configuration
      */
     Set<ConfigurationField> getConfigurations();
+
+    @Override
+    default Iterator<ConfigurationField> iterator() {
+        return this.getConfigurations().iterator();
+    }
+
+    @Override
+    default Spliterator<ConfigurationField> spliterator() {
+        return this.getConfigurations().spliterator();
+    }
+
+    @Override
+    default void forEach(Consumer<? super ConfigurationField> action) {
+        this.getConfigurations().forEach(action);
+    }
 
     class Builder {
         private final Set<ConfigurationField> fields = new HashSet<>();
